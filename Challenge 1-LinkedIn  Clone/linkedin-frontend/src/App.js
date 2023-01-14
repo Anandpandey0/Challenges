457c9efbbe15036690e43c6ec8d6d7cad49a6d6b
Anandpandey0 <75582022+Anandpandey0@users.noreply.github.com>
2023-01-15 04:29:47 +0530

    day-1 Part-1
    Created simple navbar of LinkedIn.
    But not responsive one .

---
+++ Challenge 1-LinkedIn  Clone/linkedin-frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./Header";
+
+import "./App.css";
+import Sidebar from "./Sidebar";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>Linkedin Clone</h1> */}
+      {/* Header */}
+      <Header />
+      <Sidebar />
+      {/* App Body */}
+      {/* feed */}
+      {/* Widgets */}
+    </div>
+  );
+}
+
+export default App;
