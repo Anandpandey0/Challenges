f5f8c9b26738beece0021a8056112897151eed6b
Anandpandey0 <75582022+Anandpandey0@users.noreply.github.com>
2023-05-10 09:36:24 +0530

    added vercel analytics

--- Challenge 3 Booking App/airbnb-clone/pages/_app.js
+++ Challenge 3 Booking App/airbnb-clone/pages/_app.js
@@ -1,13 +1,17 @@
 import "@/styles/globals.css";
 import { BookingProvider } from "../contexts/BookingContext";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
-    <BookingProvider>
-      <ChakraProvider>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </BookingProvider>
+    <>
+      <BookingProvider>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </BookingProvider>
+      <Analytics />
+    </>
   );
 }
